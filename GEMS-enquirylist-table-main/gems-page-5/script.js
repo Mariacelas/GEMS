bf70b144d77ed4c4f9b3fa7fbfd9d42eea35bf27
Mariacelas <57057144+Mariacelas@users.noreply.github.com>
2022-10-01 16:20:37 +0530

    Add files via upload
    Design link -
    https://xd.adobe.com/view/4c835d5a-32d1-4d26-a767-331faa9a2887-19d0/screen/5ff64fb9-2140-45be-b644-f4b28558508a/

---
+++ GEMS-enquirylist-table-main/gems-page-5/script.js
@@ -0,0 +1,21 @@
+var el = document.getElementById("sidebar");
+var toggleButton = document.getElementById("sidebarCollapse");
+
+toggleButton.onclick = function () {
+  el.classList.toggle("toggled");
+};
+var active = document.getElementById("icon-active");
+$(document).ready(function () {
+  $("#sidebarCollapse2").click(function (e) {
+    e.preventDefault();
+    // active.style.backgroundColor="#57A4FC";
+    $("#sidebar2").toggleClass("toggled");
+  });
+});
+$(function () {
+  $("#sidebarCollapse2").click(function () {
+    $(this).find("#icon-active").hasClass("iconactive")
+      ? $(this).find("#icon-active").removeClass("iconactive")
+      : $(this).find("#icon-active").addClass("iconactive");
+  });
+});
